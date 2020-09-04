c55859b7a788c999e949415a6ba94a2bf612ee28
twber <hctsai@umich.edu>
2020-09-04 18:59:22 +0800

    Initial Commit

---
+++ scissorPaperRock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scissorPaperRock
+//
+//  Created by Bernice TSAI on 2020/9/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
