e91d288a6f84ecca84ea650d410518a45f95a6b9
twber <hctsai@umich.edu>
2020-09-05 17:36:15 +0800

    playButton: chances >= 1;  @chance == 1: startOver button appears right before chances -= 1;  segmentedControl: chances >0 work countLabel: both in SegmentedControl and  button

--- scissorPaperRock/ViewController.swift
+++ scissorPaperRock/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var choiceSegmentedControl: UISegmentedControl!
     @IBOutlet weak var playerLabel: UILabel!
     @IBOutlet weak var computerLabel: UILabel!
-    var index: Int = 0
     var chances: Int  = 3
     var winCount: Int = 0
     var lossCount: Int = 0
@@ -56,39 +55,47 @@ class ViewController: UIViewController {
     }
 
     @IBAction func changeSegmentedControl(_ sender: Any) {
-        countLabel.text = "第 \(4-chances) 局"
-        computerLabel.text = "❓"
-        let playerChoice = choiceSegmentedControl.selectedSegmentIndex
-        switch playerChoice {
-        case 0: // Rock
-            playerLabel.text = punchesLabel[playerChoice]
-        case 1:// Scissor
-            playerLabel.text = punchesLabel[playerChoice]
-        case 2: //Paper
-            playerLabel.text = punchesLabel[playerChoice]
-            
-        default:
-            return
-        }
+        if chances > 0 {
+            countLabel.text = "第 \(4-chances) 局"
+            computerLabel.text = "❓"
+            let playerChoice = choiceSegmentedControl.selectedSegmentIndex
+            switch playerChoice {
+            case 0: // Rock
+                playerLabel.text = punchesLabel[playerChoice]
+            case 1:// Scissor
+                playerLabel.text = punchesLabel[playerChoice]
+            case 2: //Paper
+                playerLabel.text = punchesLabel[playerChoice]
+                
+            default:
+                return
+            }
+    }
     }
     
     @IBAction func battlePlay(_ sender: Any) {
-        if playerLabel.text != "❓"{
-        chances -= 1
         prizeLabel.isHidden = true
-        let computerChoice = Int.random(in: 0...2)
-        computerLabel.text = punchesLabel[computerChoice]
-            //let punchesLabel: [String] = ["👊", "✌️", "👋"]
-        if chances > 0{
-            whoWins(computerChoice: computerChoice)
+        if playerLabel.text != "❓"{
+            if chances >= 1 {
+                let computerChoice = Int.random(in: 0...2)
+                computerLabel.text = punchesLabel[computerChoice]
+                whoWins(computerChoice: computerChoice)
+                countLabel.text = "第 \(4-chances) 局"
+                if chances == 1{
+                    countLabel.text = "第 \(4-chances) 局 結束"
+                    clearButton.isHidden = false
+                }
+                chances -= 1
+                
+                    
         }else if chances == 0{
-            whoWins(computerChoice: computerChoice)
-            clearButton.isHidden = false
-            playButton.isEnabled = false
-            choiceSegmentedControl.isEnabled = false
-            prizeLabel.isHidden = false
-            prizeLabel.text = "\(winCount) 勝 \(lossCount) 負 \(evenCount) 和"
+            //playButton.isEnabled = false
+            //choiceSegmentedControl.isEnabled = false
+            prizeLabel.isHidden = true
         }
+        
+            //let punchesLabel: [String] = ["👊", "✌️", "👋"]
+        
     }
     }
     
